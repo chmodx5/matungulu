8aacec115b0e4cf37b7173ab3f7f1a7f05655770
chmodx5 <chmodx5@gmail.com>
2021-08-11 17:38:53 -0700

    editing insurance card

--- src/components/insuranceCard.vue
+++ src/components/insuranceCard.vue
@@ -1,6 +1,12 @@
 <template>
   <v-card tile hover height="50" outlined class="pa-0 d-flex">
-    <v-img height="100%" width="auto" :aspect-ratio="1 / 1" :src="img"> </v-img>
+    <v-img
+      height="100%"
+      :src="require(`@/assets/img/${img}`)"
+      width="auto"
+      :aspect-ratio="1 / 1"
+    >
+    </v-img>
     <div class="px-3 py-3">
       <h4 class="text-body-1 font-weight-bold text-capitalize">
         {{ title }}
