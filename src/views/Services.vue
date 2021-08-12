48db9af5801aa004e52ddbb5bfdcbda13e0e4a87
chmodx5 <chmodx5@gmail.com>
2021-08-12 07:39:59 +0300

    Services

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -24,11 +24,83 @@ export default {
       {
         id: "outpatient",
         img: "https://source.unsplash.com/j7tWLBlK9mk",
-        icon: "mdi-home",
-        title: "some title",
+        left: false,
+        icon: "mdi-hospital-building",
+        title: "outpatient ",
         desc:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. At dolorum atque sed expedita eos aut vero deleniti, vel voluptas blanditiis harum aliquid quisquam nemo laudantium aspernatur quo in quidem quia?",
+          "this is just some random desc purely because i im not creative enough to write something meaningful",
+      },
+      {
+        id: "inpatient",
+        img: "https://source.unsplash.com/j7tWLBlK9mk",
         left: true,
+        icon: "mdi-bed-outline",
+        title: "in-patient",
+        desc:
+          "this is just some random desc purely because i im not creative enough to write something meaningful",
+      },
+      {
+        id: "laboratory",
+        img: "https://source.unsplash.com/j7tWLBlK9mk",
+        left: false,
+        icon: "mdi-microscope",
+        title: "laboratory",
+        desc:
+          "this is just some random desc purely because i im not creative enough to write something meaningful",
+      },
+      {
+        id: "pharmacy",
+        img: "https://source.unsplash.com/j7tWLBlK9mk",
+        left: true,
+        icon: "mdi-pill",
+        title: "inhouse pharmacy",
+        desc:
+          "this is just some random desc purely because i im not creative enough to write something meaningful",
+      },
+      {
+        id: "maternity",
+        img: "https://source.unsplash.com/j7tWLBlK9mk",
+        left: false,
+        icon: "mdi-mother-nurse",
+        title: "maternity",
+        desc:
+          "this is just some random desc purely because i im not creative enough to write something meaningful",
+      },
+      {
+        id: "optical",
+        img: "https://source.unsplash.com/j7tWLBlK9mk",
+        left: true,
+        icon: "mdi-glasses",
+        title: "optical services",
+        desc:
+          "this is just some random desc purely because i im not creative enough to write something meaningful",
+      },
+      {
+        id: "physiotherapy",
+        img: "https://source.unsplash.com/j7tWLBlK9mk",
+        left: false,
+        icon: "mdi-human-wheelchair",
+        title: "physiotherapy",
+        desc:
+          "this is just some random desc purely because i im not creative enough to write something meaningful",
+      },
+      {
+        id: "dental",
+        img: "https://source.unsplash.com/j7tWLBlK9mk",
+        left: true,
+        icon: "mdi-tooth-outline",
+        title: "dental services",
+        desc:
+          "this is just some random desc purely because i im not creative enough to write something meaningful",
+      },
+      {
+        id: "ultrasound",
+        img: "https://source.unsplash.com/j7tWLBlK9mk",
+        left: false,
+        icon: "mdi-face-recognition",
+        title: "ultrasound",
+        desc:
+          "this is just some random desc purely because i im not creative enough to write something meaningful",
       },
     ],
   }),
