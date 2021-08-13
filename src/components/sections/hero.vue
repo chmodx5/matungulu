c1b88ebd7c5c41fd3696b4b0a1c9104d46fcba0b
chmodx5 <chmodx5@gmail.com>
2021-08-13 12:51:09 +0300

    finishing up on homepage

--- src/components/sections/hero.vue
+++ src/components/sections/hero.vue
@@ -1,45 +1,5 @@
 <template>
   <div class="pb-9">
-    <!-- <div class="m-container ">
-        <div class="m-left ">
-          <v-row
-            class="hero px-sm-6 px-md-0"
-            align="center"
-            justify="center"
-            justify-sm="start"
-            align-content="center"
-          >
-            <v-col
-              cols="12"
-              class="d-flex justify-center justify-sm-start py-2"
-            >
-              <div class="m-line"></div>
-            </v-col>
-            <v-col cols="8" offset="" class="py-2 mt-3">
-              <m-heading x-large class="text-center text-sm-left">
-                welcome to helthia bestcare services
-              </m-heading>
-            </v-col>
-            <v-col cols="12" class="mt-3 text-center text-sm-left">
-              <p class="">
-                Since 1999, we have been providing high-quality health care to
-                our city residents and guests.
-              </p>
-              <div class="mt-6">
-                <p class="">
-                  <v-btn color="primary" large>
-                    learn more
-                  </v-btn>
-                </p>
-              </div>
-            </v-col>
-          </v-row>
-        </div>
-        <div class="m-right">
-          <v-img src="@/assets/mask.jpg" height="100%" width="100%"> </v-img>
-        </div>
-      </div> 
-      -->
     <v-card elevation="2" tile flat height="600" class=" ">
       <v-carousel
         :continuous="true"
@@ -57,32 +17,34 @@
             height="100%"
             class="d-flex align-center"
           >
-            <v-container>
-              <v-row style="">
-                <v-col md="8">
-                  <h1 class="text-h2 font-weight-black">
-                    {{ heroslide.title }}
-                  </h1>
-                  <br />
-                  <h6 class="text-h6">
-                    {{ heroslide.subtitle }}
-                  </h6>
+            <v-overlay absolute color="secondary">
+              <v-container style="width:100vw">
+                <v-row style="">
+                  <v-col md="8">
+                    <h1 class="text-h2 font-weight-black">
+                      {{ heroslide.title }}
+                    </h1>
+                    <br />
+                    <h6 class="text-h6">
+                      {{ heroslide.subtitle }}
+                    </h6>
 
-                  <br />
+                    <br />
 
-                  <p>
-                    <v-btn
-                      :to="heroslide.btnlink"
-                      tile
-                      large
-                      class="font-weight"
-                      color="secondary"
-                      >{{ heroslide.btntext }}</v-btn
-                    >
-                  </p>
-                </v-col>
-              </v-row>
-            </v-container>
+                    <p>
+                      <v-btn
+                        :to="heroslide.btnlink"
+                        tile
+                        large
+                        class="font-weight"
+                        color="secondary"
+                        >{{ heroslide.btntext }}</v-btn
+                      >
+                    </p>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-overlay>
           </v-img>
         </v-carousel-item>
       </v-carousel>
@@ -171,18 +133,16 @@ export default {
         subtitle:
           "We aim to make your life better, letting you and your family live the life you want",
         img: "hero/carousel1.jpeg",
+        btntext: "book appointment",
+        btnlink: "/appointment",
       },
       {
         title: "COVID-19 is real!",
         subtitle:
           "Remember to always wear a mask, sanitize or wash your hands with soap regularly and keep social distance.",
         img: "hero/corona1.jpeg",
-      },
-      {
-        title: "COVID-19 is real!",
-        subtitle:
-          "Remember to always wear a mask, sanitize or wash your hands with soap regularly and keep social distance.",
-        img: "hero/corona1.jpeg",
+        btntext: "about us",
+        btnlink: "/about",
       },
     ],
   }),
