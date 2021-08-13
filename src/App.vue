f2446b357fb2297176caaa4c94892725bfde58ce
chmodx5 <chmodx5@gmail.com>
2021-08-13 14:33:09 +0300

    image

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@
     <nav-bar></nav-bar>
     <v-main>
       <div v-if="$route.path != '/'">
-        <v-parallax height="300" src="https://source.unsplash.com/j7tWLBlK9mk">
-        
+        <v-parallax height="300" src="@/assets/img/hero/inpatient1.jpeg">
           <v-overlay light absolute color="secondary" opacity=".5">
             <v-container style="width:100vw" class="">
               <h1 class="text-h3 font-weight-black text-capitalize">
