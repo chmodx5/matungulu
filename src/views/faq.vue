d93060d0403968cb5cca545a98bcec9d4ec189bf
chmodx5 <chmodx5@gmail.com>
2021-09-15 06:02:04 +0300

    FAQS

--- src/views/faq.vue
+++ src/views/faq.vue
@@ -23,19 +23,38 @@ export default {
   data: () => ({
     faqs: [
       {
-        question: "How can I book an appointment?",
+        question: "Is Matungulu Medical empanelled by NHIF?",
         answer:
-          "You can call us at 0700 631 291. Alternatively, you can click the 'Book Appointment' button on the top-right corner of the website and fill in the form that opens.",
+          "Yes, MMHWC is accredited by NHIF to provide both outpatient and inpatient services to NHIF beneficiaries",
       },
       {
-        question: "Where is your facility located?",
-        answer:
-          "We are located at Tala Market street, Matungulu sub-county, Machakos County. You can find our location on the ap in the 'Contacts' Page.",
+        question: "Do you accept other medical insurance cards?",
+        answer: "Yes we do.",
       },
       {
-        question: "Can I get a COVID-19 vaccine in your facility?",
+        question: "Where is the hospital located at?",
         answer:
-          "Currently, you cannot. However we can advise you where to get one.",
+          "Matungulu Medical is located in the heart of Tala Market just before the Katine Road turn-off.",
+      },
+      {
+        question: "What do I need to carry during my doctor's visit?",
+        answer:
+          "To help us serve you better, please carry the following-: NHIF Card or Insurance Card, ID Card, any previous medical records and any existing medication",
+      },
+      {
+        question: "How can I reach you in case of an emergency?",
+        answer: "You can call us through 0700631291 or 0738669259.",
+      },
+      {
+        question:
+          "If a client is not satisfied with Matungulu Medical services, what can she/he do?",
+        answer:
+          "The client should engage the hospital in-charge and register the dissatisfaction. To submit feedback, complain or suggestion, the client can contact customer care at 0738669259 or write to complaints@matungulumedical.co.ke",
+      },
+      {
+        question: "How can I become part of your workforce?",
+        answer:
+          "We post all our job openings on our website. We advise that you keep checking for any openings You can also send in your application to hr@matungulumedical.co.ke and we will get in-touch with you if an opportunity is available",
       },
     ],
   }),
